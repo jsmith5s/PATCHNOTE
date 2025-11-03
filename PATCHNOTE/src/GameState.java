978bef40795b3aea7117bab095290968d533ab3f
rblos <90212779+rblos@users.noreply.github.com>
2025-11-02 22:37:37 -0800

    initial push w/ JFrame

---
+++ PATCHNOTE/src/GameState.java
@@ -0,0 +1,49 @@
+import javax.swing.JFrame;
+/**
+* Lead Author(s):
+* @author mastahype; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2025-11-02
+*/
+
+/**
+ * Purpose: The reponsibility of GameState is ...
+ *
+ * GameState is-a ...
+ * GameState is ...
+ */
+public class GameState extends JFrame
+{
+	
+	public GameState() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setTitle("PATCHNOTE");
+		setSize(640*2, 480*2);
+		setLocationRelativeTo(null);
+	}
+
+	/**
+	 * Purpose: 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new GameState();
+
+	}
+
+}
