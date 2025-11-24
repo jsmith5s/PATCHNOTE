d6c62135cbc637fce47a895aa39e58b3f277767e
rblos <90212779+rblos@users.noreply.github.com>
2025-11-23 18:12:03 -0800

    semi-working update function, need to fix drawing on screen

--- PATCHNOTE/src/GameState.java
+++ PATCHNOTE/src/GameState.java
@@ -21,12 +21,19 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  * Purpose: The reponsibility of GameState is ...
@@ -34,13 +41,17 @@ import javax.swing.JPanel;
  * GameState is-a ...
  * GameState is ...
  */
-public class GameState extends JPanel
+public class GameState extends JPanel implements ActionListener, KeyListener
 {
 	
+	private Boolean running = true;
 	BufferedImage spriteSheet;
-	int temp;
+	Graphics2D g2d;;
 
 	public GameState() {
+		run();
+		Timer t = new Timer(16, this);
+		t.start();
 		setDoubleBuffered(true);
 		setBackground(Color.red);
 		System.out.println("success");
@@ -59,6 +70,37 @@ public class GameState extends JPanel
 		}
 	}
 	
+	public void update() {
+		//repaint();
+	}
+	
+	public void run()
+	{
+		long lastTime = System.nanoTime();
+		double delta = 0.0;
+		double ns = 1000000000.0 / 60.0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			if (delta >= 1.0) {
+				update();
+				updates++;
+				delta--;
+			}
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println(updates + " UPS, " + frames + " FPS");
+				updates = frames = 0;
+			}
+		}
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -66,8 +108,35 @@ public class GameState extends JPanel
 		Graphics2D g2d = (Graphics2D)g;
 		
 		g2d.drawImage(spriteSheet, 600, 300, 35*3, 40*3, null);
-		System.out.println(temp++);
 		//g2d.fillRect(100, 100, 200, 200);
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
 }
